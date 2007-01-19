ec53de70c40604248a3f5c66cf8987169af28fc2
galfar <none@none>
2007-01-19 12:33:22 +0000

    some stuff finally added

--- Source/ImagingColors.pas
+++ Source/ImagingColors.pas
@@ -34,9 +34,113 @@ interface
 {$I ImagingOptions.inc}
 
 uses
-  SysUtils, ImagingTypes;
+  SysUtils, ImagingTypes, ImagingUtility;
+
+{ Converts RGB color to YUV.}
+procedure RGBToYUV(R, G, B: Byte; var Y, U, V: Byte);
+{ Converts YIV to RGB color.}
+procedure YUVToRGB(Y, U, V: Byte; var R, G, B: Byte);
+
+{ Converts RGB color to YCbCr as used in JPEG.}
+procedure RGBToYCbCr(R, G, B: Byte; var Y, Cb, Cr: Byte);
+{ Converts YCbCr as used in JPEG to RGB color.}
+procedure YCbCrToRGB(Y, Cb, Cr: Byte; var R, G, B: Byte);
+{ Converts RGB color to YCbCr as used in JPEG.}
+procedure RGBToYCbCr16(R, G, B: Word; var Y, Cb, Cr: Word);
+{ Converts YCbCr as used in JPEG to RGB color.}
+procedure YCbCrToRGB16(Y, Cb, Cr: Word; var R, G, B: Word);
+
+{ Converts RGB color to CMY.}
+procedure RGBToCMY(R, G, B: Byte; var C, M, Y: Byte);
+{ Converts CMY to RGB color.}
+procedure CMYToRGB(C, M, Y: Byte; var R, G, B: Byte);
+
+{ Converts RGB color to CMYK.}
+procedure RGBToCMYK(R, G, B: Byte; var C, M, Y, K: Byte);
+{ Converts CMYK to RGB color.}
+procedure CMYKToRGB(C, M, Y, K: Byte; var R, G, B: Byte);
 
 implementation
+
+procedure RGBToYUV(R, G, B: Byte; var Y, U, V: Byte);
+begin
+  Y := ClampToByte(Round( 0.257 * R + 0.504 * G + 0.098 * B) + 16);
+  V := ClampToByte(Round( 0.439 * R - 0.368 * G - 0.071 * B) + 128);
+  U := ClampToByte(Round(-0.148 * R - 0.291 * G + 0.439 * B) + 128);
+end;
+
+procedure YUVToRGB(Y, U, V: Byte; var R, G, B: Byte);
+var
+  CY, CU, CV: LongInt;
+begin
+  CY := Y - 16;
+  CU := U - 128;
+  CV := V - 128;
+  R := ClampToByte(Round(1.164 * CY - 0.002 * CU + 1.596 * CV));
+  G := ClampToByte(Round(1.164 * CY - 0.391 * CU - 0.813 * CV));
+  B := ClampToByte(Round(1.164 * CY + 2.018 * CU - 0.001 * CV));
+end;
+
+procedure RGBToYCbCr(R, G, B: Byte; var Y, Cb, Cr: Byte);
+begin
+  Y  := ClampToByte(Round( 0.29900 * R + 0.58700 * G + 0.11400 * B));
+  Cb := ClampToByte(Round(-0.16874 * R - 0.33126 * G + 0.50000 * B  + 128));
+  Cr := ClampToByte(Round( 0.50000 * R - 0.41869 * G - 0.08131 * B  + 128));
+end;
+
+procedure YCbCrToRGB(Y, Cb, Cr: Byte; var R, G, B: Byte);
+begin
+  R := ClampToByte(Round(Y                        + 1.40200 * (Cr - 128)));
+  G := ClampToByte(Round(Y - 0.34414 * (Cb - 128) - 0.71414 * (Cr - 128)));
+  B := ClampToByte(Round(Y + 1.77200 * (Cb - 128)));
+end;
+
+procedure RGBToYCbCr16(R, G, B: Word; var Y, Cb, Cr: Word);
+begin
+  Y  := ClampToWord(Round( 0.29900 * R + 0.58700 * G + 0.11400 * B));
+  Cb := ClampToWord(Round(-0.16874 * R - 0.33126 * G + 0.50000 * B  + 32768));
+  Cr := ClampToWord(Round( 0.50000 * R - 0.41869 * G - 0.08131 * B  + 32768));
+end;
+
+procedure YCbCrToRGB16(Y, Cb, Cr: Word; var R, G, B: Word);
+begin
+  R := ClampToWord(Round(Y                          + 1.40200 * (Cr - 32768)));
+  G := ClampToWord(Round(Y - 0.34414 * (Cb - 32768) - 0.71414 * (Cr - 32768)));
+  B := ClampToWord(Round(Y + 1.77200 * (Cb - 32768)));
+end;
+
+procedure RGBToCMY(R, G, B: Byte; var C, M, Y: Byte);
+begin
+  C := 255 - R;
+  M := 255 - G;
+  Y := 255 - B;
+end;
+
+procedure CMYToRGB(C, M, Y: Byte; var R, G, B: Byte);
+begin
+  R := 255 - C;
+  G := 255 - M;
+  B := 255 - Y;
+end;
+
+procedure RGBToCMYK(R, G, B: Byte; var C, M, Y, K: Byte);
+begin
+  RGBToCMY(R, G, B, C, M, Y);
+  K := Min(C, Min(M, Y));
+  if K > 0 then
+  begin
+    C := C - K;
+    M := M - K;
+    Y := Y - K;
+  end;
+end;
+
+procedure CMYKToRGB(C, M, Y, K: Byte; var R, G, B: Byte);
+begin
+   if C + K < 255 then R := 255 - (C + K) else R := 0;
+   if M + K < 255 then G := 255 - (M + K) else G := 0;
+   if Y + K < 255 then B := 255 - (Y + K) else B := 0;
+end;
 
 {
   File Notes:
@@ -44,8 +148,12 @@ implementation
   -- TODOS ----------------------------------------------------
     - nothing now
 
+  -- 0.21 Changes/Bug Fixes -----------------------------------
+    - Added some color space conversion functions and LUTs
+      (RGB/YUV/YCrCb/CMY/CMYK).
+
   -- 0.17 Changes/Bug Fixes -----------------------------------
-    - unit created and initial functions added
+    - unit created (empty!)
 }
 
 end.
