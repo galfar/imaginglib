600ef1466e0f65ac7ceead9bdc3c780bae2e0821
Marek Mauder <none@none>
2012-06-01 02:36:08 +0200

    - dynlib, package, and demos reorganized

--- Scripts/Clean.bat
+++ Scripts/Clean.bat
@@ -10,13 +10,12 @@ del /q /f %ROOTDIR%\Demos\Bin\_BenchOut.* 2>nul 1>nul
 call :DELINTREE %ROOTDIR%\Bin
 call :DELINTREE %ROOTDIR%\Demos 
 call :DELINTREE %ROOTDIR%\Scripts 
-call :DELINTREE %ROOTDIR%\Source\Wrappers
 call :DELINTREE %ROOTDIR%\Source\JpegLib
 call :DELINTREE %ROOTDIR%\Source\ZLib
-call :DELINTREE %ROOTDIR%\Source\Extensions
-call :DELINTREE %ROOTDIR%\Source\Projects
 call :DELINDIR %ROOTDIR%\Source
 call :DELINDIR %ROOTDIR%\Extras\Extensions
+call :DELINDIR %ROOTDIR%\Extras\DynamicLib
+call :DELINDIR %ROOTDIR%\Extras\IdePackages
 call :DELINDIR %ROOTDIR%\Extras\Extensions\LibTiff
 call :DELINTREE %ROOTDIR%\Extras\Demos
 call :DELINTREE %ROOTDIR%\Extras\Tools
