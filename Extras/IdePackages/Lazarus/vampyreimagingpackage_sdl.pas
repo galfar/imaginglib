2c6c02b6cdebf2cfb6370a6a17034e2dce291228
wp-xyz <wp_xxyyzzz@gmx.net>
2018-03-12 17:06:16 +0100

    Add packages for Direct3D and SDL add-ons

---
+++ Extras/IdePackages/Lazarus/vampyreimagingpackage_sdl.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit vampyreimagingpackage_sdl;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ImagingSDL, sdl, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('vampyreimagingpackage_sdl', @Register);
+end.
