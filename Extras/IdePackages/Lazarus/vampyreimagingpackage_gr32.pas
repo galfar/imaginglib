b15ea7bfbcef731ea2ef7b8894d1266a9b91608b
wp-xyz <wp_xxyyzzz@gmx.net>
2018-03-15 17:29:25 +0100

    - Split package VampyreImagingPackage_Ext off of main package. - Dto. with Graphics32, OpenGL, Direct3D and SDL add-ons. - Fix FPC not using Vampyre's graphics classes when accessing images from resource (see https://forum.lazarus.freepascal.org/index.php/topic,34297.msg279182.html#msg279182).

---
+++ Extras/IdePackages/Lazarus/vampyreimagingpackage_gr32.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit vampyreimagingpackage_gr32;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ImagingGraphics32, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('vampyreimagingpackage_gr32', @Register);
+end.
