1b348bd64d995ae8da817068806eadce03bc8f9f
Marek Mauder <marekmauder@gmail.com>
2022-01-24 00:26:38 +0100

    Fixed #21: Broken TImagingGraphic VCL/LCL component streaming
    ReadData and WriteData of TImagingGraphic+TImagingGraphicForSave were not properly implemented wreaking havoc (albeit different kind in Delphi and Lazarus) when used in TImage(TPicture) on form (and imaginglib IDE package installed).

--- Source/ImagingComponents.pas
+++ Source/ImagingComponents.pas
@@ -37,7 +37,6 @@ uses
   Graphics,
 {$ENDIF}
 {$IFDEF COMPONENT_SET_LCL}
-  InterfaceBase,
   GraphType,
   Graphics,
   LCLType,
@@ -49,17 +48,27 @@ type
   { Graphic class which uses Imaging to load images.
     It has standard TBitmap class as ancestor and it can
     Assign also to/from TImageData structures and TBaseImage
-    classes. For saving is uses inherited TBitmap methods.
+    classes. If you want to perfectly preserve the original pixel format
+    of the source image then these classes may not for you.
+
     This class is automatically registered to TPicture for all
     file extensions supported by Imaging (useful only for loading).
     If you just want to load images in various formats you can use this
-    class or simply use  TPicture.LoadFromXXX which will create this class
-    automatically. For TGraphic class that saves with Imaging look
+    class or simply use TPicture.LoadFromXXX which will create this class
+    automatically.
+
+    For saving it uses inherited TBitmap methods
+    (it saves the image in BMP format regardless of extension you request).
+    For TGraphic class that saves with Imaging look
     at TImagingGraphicForSave class.}
   TImagingGraphic = class(TBitmap)
   protected
-    procedure ReadDataFromStream(Stream: TStream); virtual;
     procedure AssignTo(Dest: TPersistent); override;
+    { Called by TFiler when reading and writing TPicture.Data property.
+      We need to override ReadData+WriteData otherwise inherited ones from
+      TBitmap would be called resulting in errors.}
+    procedure ReadData(Stream: TStream); override;
+    procedure WriteData(Stream: TStream); override;
   public
     constructor Create; override;
 
@@ -79,25 +88,26 @@ type
     procedure AssignFromImageData(const ImageData: TImageData);
     { Copies the current image to TImageData structure.}
     procedure AssignToImageData(var ImageData: TImageData);
-
-  {$IFDEF FPC}
+  {$IFDEF COMPONENT_SET_LCL}
     class function IsStreamFormatSupported(Stream: TStream): boolean; override;
   {$ENDIF}
   end;
 
   TImagingGraphicClass = class of TImagingGraphic;
 
-  { Base class for file format specific TGraphic classes that use
+  { Base (abstract) class for file format specific TGraphic classes that use
     Imaging for saving. Each descendant class can load all file formats
     supported by Imaging but save only one format (TImagingBitmap
-    for *.bmp, TImagingJpeg for *.jpg). Format specific classes also
-    allow easy access to Imaging options that affect saving of files
-    (they are properties here).}
+    for *.bmp, TImagingJpeg for *.jpg). The image is saved in this one file
+    format regardless of the extension you request).
+
+    Format specific classes also allow easy access to Imaging options that
+    affect saving of files (they are properties here).}
   TImagingGraphicForSave = class(TImagingGraphic)
   protected
     FDefaultFileExt: string;
     FSavingFormat: TImageFormat;
-    procedure WriteDataToStream(Stream: TStream); virtual;
+    procedure WriteData(Stream: TStream); override;
   public
     constructor Create; override;
     { Saves the current image to the stream. It is saved in the
@@ -195,7 +205,7 @@ type
 {$ENDIF}
 
 {$IFNDEF DONT_LINK_DDS}
-  { Compresssion type used when saving DDS files by TImagingDds.}
+  { Compression type used when saving DDS files by TImagingDds.}
   TDDSCompression = (dcNone, dcDXT1, dcDXT3, dcDXT5);
 
   { TImagingGraphic descendant for loading/saving DDS images.}
@@ -331,7 +341,7 @@ implementation
 uses
 {$IF Defined(LCL)}
   {$IF Defined(LCLGTK2)}
-    GLib2, GDK2, GTK2, GTK2Def, GTK2Proc,
+    InterfaceBase, GLib2, GDK2, GTK2, GTK2Def, GTK2Proc,
   {$IFEND}
 {$IFEND}
 {$IFNDEF DONT_LINK_BITMAP}
@@ -904,12 +914,27 @@ begin
   PixelFormat := pf24Bit;
 end;
 
-procedure TImagingGraphic.LoadFromStream(Stream: TStream);
+procedure TImagingGraphic.ReadData(Stream: TStream);
 begin
-  ReadDataFromStream(Stream);
+  // Here we need to skip ReadData+WriteData of TBitmap (and LCL TRasterBitmap)
+  // and go to the basics in TGraphic's ReadData+WriteData with just LoadFromStream
+  // and SaveToStream.
+  // Some VCL/LCL TGraphic classes also store size of the written data
+  // before the stream contents. However, the stream passed here
+  // from TReader.DefineBinaryProperty is already
+  // a memory stream capped to the size of binary property data.
+  // Picture.Data = <vaBinary><Size(TWriter)><TGraphicClassName(TPicture)><ImageBits(TImagingGraphicForSave)>
+  LoadFromStream(Stream);
 end;
 
-procedure TImagingGraphic.ReadDataFromStream(Stream: TStream);
+procedure TImagingGraphic.WriteData(Stream: TStream);
+begin
+  // This should never occur in the wild since IDE would create one of the descendants
+  // of TImagingGraphicForSave for specific format+extension when e.g. setting TPicture of TImage.
+  raise ENotImplemented.CreateFmt('Not implemented: Use TImagingGraphicForSave class for writing with TFiler.', []);
+end;
+
+procedure TImagingGraphic.LoadFromStream(Stream: TStream);
 var
   Image: TSingleImage;
 begin
@@ -941,7 +966,7 @@ begin
     inherited AssignTo(Dest);
 end;
 
-{$IFDEF FPC}
+{$IFDEF COMPONENT_SET_LCL}
 class function TImagingGraphic.IsStreamFormatSupported(Stream: TStream): Boolean;
 begin
   Result := DetermineStreamFormat(Stream) <> '';
@@ -991,7 +1016,12 @@ begin
   GetFileFormat.CheckOptionsValidity;
 end;
 
-procedure TImagingGraphicForSave.WriteDataToStream(Stream: TStream);
+procedure TImagingGraphicForSave.WriteData(Stream: TStream);
+begin
+  SaveToStream(Stream);
+end;
+
+procedure TImagingGraphicForSave.SaveToStream(Stream: TStream);
 var
   Image: TSingleImage;
 begin
@@ -1007,11 +1037,6 @@ begin
       Image.Free;
     end;
   end;
-end;
-
-procedure TImagingGraphicForSave.SaveToStream(Stream: TStream);
-begin
-  WriteDataToStream(Stream);
 end;
 
 {$IFDEF COMPONENT_SET_LCL}
@@ -1262,9 +1287,6 @@ finalization
 
 {
   File Notes:
-
-  -- TODOS ----------------------------------------------------
-    - nothing now
 
   -- 0.77.1 ---------------------------------------------------
     - Fixed bug in ConvertBitmapToData causing images from GTK2 bitmaps
