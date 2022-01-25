cd80f8e29f5d34c7dc93ff1b38cdee5fdbe871e9
Marek Mauder <marekmauder@gmail.com>
2022-01-25 03:00:35 +0100

    Lazarus packages updated

--- Extras/Packages/VampyreImagingPackage_SDL.pas
+++ Extras/Packages/VampyreImagingPackage_SDL.pas
@@ -2,7 +2,7 @@
   This source is only used to compile and install the package.
  }
 
-unit VampyreImagingPackage_Sdl;
+unit VampyreImagingPackage_SDL;
 
 {$warn 5023 off : no warning about unused units}
 interface
