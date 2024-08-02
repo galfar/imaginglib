298fb3903e4c0dc7ec82f568e89795a9a1181ac1
galfar <marekmauder@gmail.com>
2024-08-03 01:22:37 +0200

    ImagingFmx fixes
    - rework of CopyRectToFmxBitmap
    - source and destination positions were mostly broken
    - less code
    - added alpha premultiplication: needed for FMX bitmaps to display correctly in TImage etc.

--- Extensions/ImagingFmx.pas
+++ Extensions/ImagingFmx.pas
@@ -7,16 +7,21 @@
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at https://mozilla.org/MPL/2.0.
-} 
+}
 
-{ Functions and classes for interoperability between Imaging and
-  FireMonkey framework.}
+{
+  Functions and classes for interoperability between Imaging and
+  FireMonkey framework.
+
+  Currently has conversion functions between FMX TBitmap (RGBA and BGRA pixel formats)
+  and Imaging's TImageData and TBaseImage.
+}
 unit ImagingFmx;
 
 {$I ImagingOptions.inc}
 
 {$IF not Defined (DCC) or (CompilerVersion < 25.0)}
-  {$MESSAGE FATAL 'Needs at least Delphi XE4'}
+  {$MESSAGE FATAL 'Needs at least Delphi XE4, probably higher'}
 {$IFEND}
 
 interface
@@ -30,17 +35,20 @@ uses
   ImagingClasses,
   ImagingUtility,
   UITypes,
-  Fmx.Types,
-  Fmx.Utils,
-  Fmx.Graphics;
+  UIConsts,
+  FMX.Types,
+  FMX.Utils,
+  FMX.Graphics;
 
 { Converts image from TImageData record to FMX bitmap. Bitmap must be already instantiated.}
 procedure ConvertImageDataToFmxBitmap(const Image: TImageData; Bitmap: TBitmap);
-{ Converts FMX bitmap to TImageData. Image Data must already instantiated.}
+{ Converts FMX bitmap to TImageData. Image must be already initialized.}
 procedure ConvertFmxBitmapToImageData(const Bitmap: TBitmap; Image: TImageData);
 
 { Converts image from TBaseImage instance to FMX bitmap. Bitmap must be already instantiated.}
 procedure ConvertImageToFmxBitmap(Image: TBaseImage; Bitmap: TBitmap);
+{ Converts FMX bitmap to TBaseImage. Image must be already instantiated.}
+procedure ConvertFmxBitmapToImage(const Bitmap: TBitmap; Image: TBaseImage);
 
 { Copies rectangular area of pixels from TImageData record to existing FMX bitmap.}
 procedure CopyRectToFmxBitmap(const Image: TImageData; Bitmap: TBitmap;
@@ -60,6 +68,8 @@ var
   X, Y, Bpp, SrcWidthBytes: Integer;
   TargetInfo: TImageFormatInfo;
 begin
+  Assert(TestImage(Image) and not Bitmap.IsEmpty);
+
   Bitmap.Map(TMapAccess.Read, MapData);
   GetImageFormatInfo(Image.Format, TargetInfo);
 
@@ -68,7 +78,7 @@ begin
   TargetData := @PByteArray(Image.Bits)[0];
 
   for Y := 0 to Bitmap.Height - 1 do
-   for X:= 0 to Bitmap.Width - 1 do
+   for X := 0 to Bitmap.Width - 1 do
     begin
       SourceData:= @PAlphaColorRecArray(MapData.Data)[Y * (MapData.Pitch div 4) + X];
       case TargetInfo.Format of
@@ -126,6 +136,11 @@ begin
   Bitmap.Unmap(MapData);
 end;
 
+procedure ConvertFmxBitmapToImage(const Bitmap: TBitmap; Image: TBaseImage);
+begin
+  ConvertFmxBitmapToImageData(Bitmap, Image.ImageDataPointer^);
+end;
+
 procedure ConvertImageDataToFmxBitmap(const Image: TImageData; Bitmap: TBitmap);
 begin
   Assert(TestImage(Image));
@@ -141,15 +156,15 @@ end;
 procedure ConvertToAlphaColorRec(SrcPix: PByte; DestPix: PAlphaColorRec;
   const SrcInfo: TImageFormatInfo; SrcPalette: PPalette32);
 var
-   Color32:TColor32Rec;
+  Color32: TColor32Rec;
 begin
   case SrcInfo.Format of
     ifIndex8:
       begin
-        DestPix^.R := SrcPalette[SrcPix^].R;
-        DestPix^.G := SrcPalette[SrcPix^].G;
-        DestPix^.B := SrcPalette[SrcPix^].B;
-        DestPix^.A := SrcPalette[SrcPix^].A;
+        DestPix.R := SrcPalette[SrcPix^].R;
+        DestPix.G := SrcPalette[SrcPix^].G;
+        DestPix.B := SrcPalette[SrcPix^].B;
+        DestPix.A := SrcPalette[SrcPix^].A;
       end;
     ifGray8:
       begin
@@ -158,54 +173,15 @@ begin
         DestPix.B := SrcPix^;
         DestPix.A := 255;
       end;
-    ifA8Gray8:
-      begin
-        DestPix.R := SrcPix^;
-        DestPix.G := SrcPix^;
-        DestPix.B := SrcPix^;
-        DestPix.A := PWordRec(SrcPix).High;
-      end;
-    ifGray16:
-      begin
-        DestPix.R := PWord(SrcPix)^ shr 8;
-        DestPix.G := DestPix.R;
-        DestPix.B := DestPix.R;
-        DestPix.A := 255;
-      end;
     ifR8G8B8:
       begin
-        DestPix.R := PColor24Rec(SrcPix)^.R;
-        DestPix.G := PColor24Rec(SrcPix)^.G;
-        DestPix.B := PColor24Rec(SrcPix)^.B;
+        PColor24Rec(DestPix)^ := PColor24Rec(SrcPix)^;
         DestPix.A := 255;
       end;
     ifA8R8G8B8:
-      begin
-        DestPix^.R := PColor32Rec(SrcPix)^.R;
-        DestPix^.G := PColor32Rec(SrcPix)^.G;
-        DestPix^.B := PColor32Rec(SrcPix)^.B;
-        DestPix^.A := PColor32Rec(SrcPix)^.A;
-      end;
-    ifR16G16B16:
-      begin
-        DestPix.R := PColor48Rec(SrcPix).R shr 8;
-        DestPix.G := PColor48Rec(SrcPix).G shr 8;
-        DestPix.B := PColor48Rec(SrcPix).B shr 8;
-        DestPix.A := 255;
-      end;
-    ifA16R16G16B16:
-      begin
-        DestPix.R := PColor64Rec(SrcPix).R shr 8;
-        DestPix.G := PColor64Rec(SrcPix).G shr 8;
-        DestPix.B := PColor64Rec(SrcPix).B shr 8;
-        DestPix.A := PColor64Rec(SrcPix).A shr 8;
-      end;
+      PColor32Rec(DestPix)^ := PColor32Rec(SrcPix)^;
   else
-    Color32:=SrcInfo.GetPixel32(SrcPix, @SrcInfo, SrcPalette);
-    DestPix^.R := Color32.R;
-    DestPix^.G := Color32.G;
-    DestPix^.B := Color32.B;
-    DestPix^.A := Color32.A;
+    PColor32Rec(DestPix)^ := SrcInfo.GetPixel32(SrcPix, @SrcInfo, SrcPalette);
   end;
 end;
 
@@ -213,54 +189,48 @@ procedure CopyRectToFmxBitmap(const Image: TImageData; Bitmap: TBitmap;
   SrcX, SrcY, Width, Height, DstX, DstY: LongInt);
 var
   TempImage: TImageData;
-  X, Y, Bpp, SrcWidthBytes, MoveBytes: Integer;
+  X, Y, Bpp, SrcLineBytes: Integer;
   SrcPtr: PByte;
   Info: TImageFormatInfo;
   MapData: TBitmapData;
   DstPtr: PAlphaColorRec;
-  ARGB: TAlphaColorRec;
+  BGRA: TAlphaColorRec;
 begin
   Assert(TestImage(Image) and not Bitmap.IsEmpty);
 
-  ClipCopyBounds(SrcX, SrcY, Width, Height, DstX, DstY, Image.Width, Image.Height,
-    Rect(0, 0, Bitmap.Width, Bitmap.Height));
+  if not (Bitmap.PixelFormat in [TPixelFormat.RGBA, TPixelFormat.BGRA]) then
+      raise Exception.CreateFmt('Unsupported FMX TBitmap pixel format "%s"', [PixelFormatToString(Bitmap.PixelFormat)]);
+
+  ClipCopyBounds(SrcX, SrcY, Width, Height, DstX, DstY, Image.Width,
+    Image.Height, Rect(0, 0, Bitmap.Width, Bitmap.Height));
   GetImageFormatInfo(Image.Format, Info);
 
   if not Info.IsSpecial then
   begin
     Bpp := Info.BytesPerPixel;
-    SrcWidthBytes := Image.Width * Bpp;
-    MoveBytes := Width * Bpp;
-    SrcPtr := @PByteArray(Image.Bits)[SrcY * SrcWidthBytes + SrcX * Bpp];
-    Bitmap.Map(TMapAccess.ReadWrite, MapData);
+    SrcLineBytes := Image.Width * Bpp;
+    Bitmap.Map(TMapAccess.Write, MapData);
 
     for Y := 0 to Height - 1 do
     begin
-      if Info.Format = ifA8R8G8B8 then
-      begin
-       for X := 0 to Width - 1 do
-        begin
-          DstPtr := @PColor32RecArray(MapData.Data)[Y * (MapData.Pitch div 4) + X];
-          Move(SrcPtr^, ARGB, 4);
+      SrcPtr := @PByteArray(Image.Bits)[(SrcY + Y) * SrcLineBytes + SrcX * Bpp];
+      DstPtr := @PAlphaColorRecArray(MapData.GetScanline(DstY + Y))[DstX];
 
-          if MapData.PixelFormat = TPixelFormat.RGBA then
-            SwapValues(ARGB.R, ARGB.B);
-
-          DstPtr^.A := ARGB.A;
-          DstPtr^.R := ARGB.R;
-          DstPtr^.G := ARGB.G;
-          DstPtr^.B := ARGB.B;
-          Inc(SrcPtr, 4);
-        end;
-      end
-      else
+      for X := 0 to Width - 1 do
       begin
-        for X := 0 to Width - 1 do
-        begin
-          DstPtr := @PColor32RecArray(MapData.Data)[Y * (MapData.Pitch div 4)+X];
-          ConvertToAlphaColorRec(SrcPtr, DstPtr, Info, Image.Palette);
-          Inc(SrcPtr, Bpp);
-        end;
+        if Info.Format = ifA8R8G8B8 then
+          BGRA := PAlphaColorRec(SrcPtr)^
+        else
+          ConvertToAlphaColorRec(SrcPtr, @BGRA, Info, Image.Palette);
+
+        if MapData.PixelFormat = TPixelFormat.RGBA then
+          SwapValues(BGRA.R, BGRA.B);
+
+        // Alpha premultiplication is needed for FMX bitmaps to display correctly in TImage etc.
+        DstPtr.Color := PremultiplyAlpha(BGRA.Color);
+
+        Inc(SrcPtr, Bpp);
+        Inc(DstPtr);
       end;
     end;
   end
@@ -275,7 +245,7 @@ begin
       FreeImage(TempImage);
     end;
   end;
-  Bitmap.UnMap(MapData);
+  Bitmap.Unmap(MapData);
 end;
 
 procedure CopyRectToFmxBitmap(Image: TBaseImage; Bitmap: TBitmap;
@@ -284,16 +254,5 @@ begin
   CopyRectToFmxBitmap(Image.ImageDataPointer^, Bitmap,
     SrcX, SrcY, Width, Height, DstX, DstY);
 end;
-
-{
-  File Notes:
-
-  -- 0.77.1 Changes/Bug Fixes ---------------------------------
-    - Removed support for old FMX versions (XE2 etc.)
-    - Support for current FMX version (XE4+) contributed by Ken Schafer.
-
-  -- 0.77 Changes/Bug Fixes -----------------------------------
-    - Unit created with initial stuff, working with FMX1 in Delphi XE2.
- }
 
 end.
